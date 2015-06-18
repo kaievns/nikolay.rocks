8e488bc68948af172f79ad7e42a88c781fcac388
Nikolay Nemshilov <nemshilov@gmail.com>
2015-06-18 21:34:02 +1000

    Optimizing the shaify gulp command to use the actual SHA1 checksums instead of random slugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,13 @@
-var gulp         = require("gulp");
-var browserify   = require("browserify");
-var babelify     = require("babelify");
-var sourcemaps   = require("gulp-sourcemaps");
-var source       = require("vinyl-source-stream");
-var connect      = require("gulp-connect");
-var uglify       = require('gulp-uglify');
-var replace      = require('gulp-replace');
-var buffer       = require('vinyl-buffer');
-var less         = require("gulp-less");
-var autoprefixer = require("gulp-autoprefixer");
-var minifyCss    = require('gulp-minify-css');
-var rename       = require('gulp-rename');
-var atomfeed     = require('./app/utils/atomfeed');
-var settings     = require('./app/stores/settings');
-var server       = require('./app/utils/server');
+var gulp = require("gulp");
 
 gulp.task("scripts", function() {
+  var buffer     = require('vinyl-buffer');
+  var browserify = require("browserify");
+  var babelify   = require("babelify");
+  var source     = require("vinyl-source-stream");
+  var uglify     = require('gulp-uglify');
+  var sourcemaps = require("gulp-sourcemaps");
+
   browserify({
       entries: ["./app/application.jsx"],
       extensions: [".js", ".jsx"],
@@ -32,6 +24,11 @@ gulp.task("scripts", function() {
 });
 
 gulp.task("stylesheets", function() {
+  var less         = require("gulp-less");
+  var autoprefixer = require("gulp-autoprefixer");
+  var minifyCss    = require('gulp-minify-css');
+  var sourcemaps   = require("gulp-sourcemaps");
+
   gulp.src("app/application.less")
     .pipe(sourcemaps.init())
     .pipe(less({paths: ["./app", "./bower_components/pain.less.css/src"]}))
@@ -42,6 +39,9 @@ gulp.task("stylesheets", function() {
 });
 
 gulp.task("atomfeed", function() {
+  var atomfeed = require('./app/utils/atomfeed');
+  var settings = require('./app/stores/settings');
+
   gulp.src(['./pages/**/*.md'])
     .pipe(atomfeed(settings))
     .pipe(gulp.dest("./"));
@@ -49,22 +49,27 @@ gulp.task("atomfeed", function() {
 
 
 gulp.task("watch", function() {
-  gulp.watch("./app/**/*.js"         , ["scripts"]);
-  gulp.watch("./app/**/*.jsx"        , ["scripts"]);
-  gulp.watch("./app/application.less", ["stylesheets"]);
-  gulp.watch("./pages/**/*.md"       , ["atomfeed"]);
-  gulp.watch("./application.js"      , ["livereload"]);
-  gulp.watch("./application.css"     , ["livereload"]);
-  gulp.watch("./atom.xml"            , ["livereload"]);
-  gulp.watch("./*.html"              , ["livereload"]);
+  window.setTimeout(function() {
+    gulp.watch("./app/**/*.js"         , ["scripts"]);
+    gulp.watch("./app/**/*.jsx"        , ["scripts"]);
+    gulp.watch("./app/application.less", ["stylesheets"]);
+    gulp.watch("./pages/**/*.md"       , ["atomfeed"]);
+    gulp.watch("./application.js"      , ["livereload"]);
+    gulp.watch("./application.css"     , ["livereload"]);
+    gulp.watch("./atom.xml"            , ["livereload"]);
+    gulp.watch("./*.html"              , ["livereload"]);
+  }, 1000); // waiting for the initial compile
 });
 
 gulp.task("livereload", function() {
+  var connect = require("gulp-connect");
+
   gulp.src("./index.html")
     .pipe(connect.reload());
 });
 
 gulp.task("connect", function() {
+  var server = require('./app/utils/server');
   server.start({
     root:       "./",
     port:       process.env.PORT || 8080,
@@ -73,22 +78,28 @@ gulp.task("connect", function() {
 });
 
 gulp.task("shaify", function() {
-  var slug = Math.random().toString(36).substring(3, 8);
+  var fs          = require("fs");
+  var crypto      = require("crypto");
+  var scripts     = fs.readFileSync("./application.js");
+  var styles      = fs.readFileSync("./application.css");
+  var scripts_sha = crypto.createHash("sha1").update(scripts);
+  var styles_sha  = crypto.createHash("sha1").update(styles);
+  var scripts_id  = scripts_sha.digest("base64").replace(/[^a-z0-9]+/g, "").substr(0,6);
+  var styles_id   = styles_sha.digest("base64").replace(/[^a-z0-9]+/g, "").substr(0,6);
+  var new_scripts = "application-"+scripts_id+".js";
+  var new_styles  = "application-"+styles_id+".css";
 
-  gulp.src("application.js")
-    .pipe(replace("\n//# sourceMappingURL=application.js.map", ""))
-    .pipe(rename("application-"+ slug +".js"))
-    .pipe(gulp.dest("./"));
 
-  gulp.src("application.css")
-    .pipe(replace("\n/*# sourceMappingURL=application.css.map */", ""))
-    .pipe(rename("application-"+ slug +".css"))
-    .pipe(gulp.dest("./"));
+  fs.writeFile(new_scripts, scripts.toString().replace("\n//# sourceMappingURL=application.js.map", ""));
+  fs.writeFile(new_styles, styles.toString().replace("\n/*# sourceMappingURL=application.css.map */", ""));
 
-  gulp.src("index.html")
-    .pipe(replace("/application.js", "/application-"+slug+".js"))
-    .pipe(replace("/application.css", "/application-"+slug+".css"))
-    .pipe(gulp.dest("./"));
+  fs.readFile("./index.html", function(err, data) {
+    data = data.toString();
+    data = data.replace("/application.js", "/"+ new_scripts);
+    data = data.replace("/application.css", "/"+ new_styles);
+
+    fs.writeFile("./index.html", data);
+  });
 });
 
 gulp.task("build", ["scripts", "stylesheets", "atomfeed"]);
